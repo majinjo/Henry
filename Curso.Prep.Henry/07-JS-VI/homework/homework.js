0c0ffd22fa51f7273f4e4ce0a2a731ad4661f9cc
majinjo <69877424+majinjo@users.noreply.github.com>
2020-08-28 08:39:48 -0300

    Add files via upload

---
+++ Curso.Prep.Henry/07-JS-VI/homework/homework.js
@@ -0,0 +1,45 @@
+// Do not change any of the function names
+
+function invocarCallback(cb) {
+  // Invoca al callback `cb`
+  cb();
+}
+
+function sumarArray(numeros, cb) {
+  // Suma todos los números enteros (int/integers) de un array ("numeros")
+  // Pasa el resultado a `cb`
+  // No es necesario devolver nada
+  var numeros = [1 ,2 ,3 ,4 ,5 ];
+  var sumador = 0;
+  for(var i = 0; i < numeros.length; i++) {
+  sumador = sumador + numeros[i];
+  }
+  cb(sumador);
+}
+function forEach(array, cb) {
+  // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
+  // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
+  for(var i = 0;i < array.length; i++) {
+    cb(array[i]);
+  }
+}
+
+function map(array, cb) {
+  // Crea un nuevo array
+  // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
+  // El nuevo array debe tener la misma longitud que el array del argumento
+  var nuevoarray = array.map(function(elemento) {
+  return cb(elemento);
+});
+  return nuevoarray;
+}
+
+// No modificar nada debajo de esta línea
+// --------------------------------
+
+module.exports = {
+  invocarCallback,
+  sumarArray,
+  forEach,
+  map,
+};
---
+++ Curso.Prep.Henry/README.md
@@ -0,0 +1,21 @@
+<img  src='./logo.png' height='70px'>
+
+## Introducción
+
+# Modulo 0 - Prep
+
+* [01 - Git](./01-Git) (3 hs)
+* [02 - I - Introducción a Javascript: Variables, tipos de datos y funciones](./02-JS-I) (3 hs)
+* [03 - II- Flujos de control, operadores de comparación, bucles for](./03-JS-II) (3 hs)
+* [04 - III - continuación de bucles for y Arrays](./04-JS-III) (3 hs)
+* [05 - IV - Objetos](./05-JS-IV) (3 hs)
+* [06 - V - Clases y prototype](./06-JS-V) (3 hs)
+* [07 - VI - Callbacks](./07-JS-VI) (3 hs)
+* [08 - HTML](./08-HTML) (3 hs)
+* [09 - CSS](./09-CSS-Positioning) (3 hs)
+* Checkpoint
+
+# La Carrera
+
+Si querés saber más sobre la carrera, mirá este video:
+<iframe src="https://player.vimeo.com/video/426051769" width="640" height="360" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
